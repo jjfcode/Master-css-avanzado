e11ec7ad595bf8f60c9f7f3d89ed273c55675822
John Ferlito <jjfcode@gmail.com>
2023-07-23 13:30:39 -0400

    109 Toggle de iconos en el menu

--- 06-maquetacion-web1/assets/js/menu.js
+++ 06-maquetacion-web1/assets/js/menu.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", (event) => {
 
     let button = document.querySelector(".layout__menu-toggle");
+    let icon_bars = document.querySelector(".layout__menu-toggle .fa-bars");
+    let icon_xmark = document.querySelector(".layout__menu-toggle .fa-xmark");
     let aside = document.querySelector(".layout__aside");
 
     button.addEventListener("click", (event) => {
@@ -9,8 +11,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
         if(!visible){
             aside.classList.add("layout__aside--visible");
+            icon_bars.style.opacity = 1;
+            icon_xmark.style.opacity = 0;
         }else{
             aside.classList.remove("layout__aside--visible")
+            icon_bars.style.opacity = 0;
+            icon_xmark.style.opacity = 1;
         }
 
     });
